d83dbdc385d5b86d6b738942bdfbec5c9801a545
dangkhoa-huynh <dangkhoa.huynh@onpoint.vn>
2025-11-24 10:59:18 +0700

    feat: implement Knight piece movement logic
    - Override GeneratePseudoLegalMoves() method in Knight class
    
    - Implement 8 possible L-shaped moves: (2,1), (2,-1), (-2,1), (-2,-1), (1,2), (1,-2), (-1,2), (-1,-2)
    
    - Add proper bounds checking before adding moves
    
    - Handle empty squares (Normal move) and enemy pieces (Capture move)
    
    - Skip squares occupied by friendly pieces
    
    - Remove unused Start() and Update() methods
    
    - Follow consistent code style with other piece classes (Queen, Bishop, Rock)

--- Assets/Core/Pieces/Knight.cs
+++ Assets/Core/Pieces/Knight.cs
@@ -1,16 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight : Piece
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override List<Move> GeneratePseudoLegalMoves()
     {
-        
-    }
+        List<Move> result = new List<Move>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int[] dx = { 2, 2, -2, -2, 1, 1, -1, -1 };
+        int[] dy = { 1, -1, 1, -1, 2, -2, 2, -2 };
+
+        for (int i = 0; i < 8; i++)
+        {
+            BoardPosition targetPos = new BoardPosition(pos.x + dx[i], pos.y + dy[i]);
+            
+            if (!targetPos.InBounds()) continue;
+
+            Piece targetPiece = boardManager.GetPieceAt(targetPos);
+            
+            if (targetPiece == null)
+            {
+                result.Add(new Move(this.Pos, targetPos, MoveType.Normal));
+            }
+            else if (targetPiece.Color != this.color)
+            {
+                result.Add(new Move(this.Pos, targetPos, MoveType.Capture, targetPiece));
+            }
+        }
+
+        return result;
     }
 }
