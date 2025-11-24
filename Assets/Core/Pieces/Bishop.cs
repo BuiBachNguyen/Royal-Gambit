2bc5973a382f902fe5534d60bccfce77ec3009b1
dangkhoa-huynh <dangkhoa.huynh@onpoint.vn>
2025-11-24 10:54:24 +0700

    feat: implement Bishop piece movement logic
    Add GeneratePseudoLegalMoves() method for Bishop class
    
    - Bishop can move diagonally in 4 directions
    
    - Implements sliding movement pattern along diagonal lines
    
    - Handles normal moves and capture moves correctly
    
    - Stops when encountering own pieces or board boundaries

--- Assets/Core/Pieces/Bishop.cs
+++ Assets/Core/Pieces/Bishop.cs
@@ -1,16 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Bishop : Piece
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override List<Move> GeneratePseudoLegalMoves()
     {
-        
-    }
+        List<Move> result = new List<Move>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int[] dx = { -1, -1, 1, 1 };
+        int[] dy = { -1, 1, -1, 1 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int step = 1; step < 8; step++)
+            {
+                BoardPosition targetPos = new BoardPosition(pos.x + dx[i] * step, pos.y + dy[i] * step);
+                
+                if (!targetPos.InBounds()) break;
+
+                Piece targetPiece = boardManager.GetPieceAt(targetPos);
+                
+                if (targetPiece == null)
+                {
+                    result.Add(new Move(this.Pos, targetPos, MoveType.Normal));
+                }
+                else
+                {
+                    if (targetPiece.Color != this.color)
+                    {
+                        result.Add(new Move(this.Pos, targetPos, MoveType.Capture, targetPiece));
+                    }
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
