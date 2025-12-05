45c503825547b90dc53e254fee7862dfe190956d
dangkhoa-huynh <dangkhoa.huynh@onpoint.vn>
2025-12-05 10:26:30 +0700

    refactor: optimize Board and BoardManager implementation
    - Refactor Board.cs: improve tile management and board state handling
    - Refactor Tile.cs: minor improvements to tile logic
    - Refactor BoardManager.cs: reduce complexity and improve code organization
    - Remove redundant code and improve performance

--- Assets/Core/Managers/BoardManager.cs
+++ Assets/Core/Managers/BoardManager.cs
@@ -1,198 +1,11 @@
-﻿//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.EventSystems;
-
-//public class BoardManager : MonoBehaviour
-//{
-//    #region Singleton
-//    public static BoardManager Instance { get; private set; }
-//    private void Awake()
-//    {
-//        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
-//        Instance = this;
-//        DontDestroyOnLoad(gameObject);
-//    }
-//    #endregion
-
-//    [SerializeField] private Piece[,] pieces = new Piece[8, 8];
-
-//    private Piece pickingPiece;
-//    private PlayerColor currentTurn = PlayerColor.White;
-//    private Move lastMove;
-
-//    public Piece PickingPiece { get => pickingPiece; set => pickingPiece = value; }
-//    public PlayerColor CurrentTurn { get => currentTurn; set => currentTurn = value; }
-
-//    private void OnEnable() => Piece.OnAnyPieceClicked += HandlePieceClick;
-//    private void OnDisable() => Piece.OnAnyPieceClicked -= HandlePieceClick;
-
-//    private void HandlePieceClick(Piece clickedPiece, PointerEventData eventData)
-//    {
-//        if (clickedPiece.Color != currentTurn) return;
-
-//        pickingPiece = clickedPiece;
-//        var pseudo = clickedPiece.GeneratePseudoLegalMoves();
-//        var legal = FilterLegalMoves(clickedPiece, pseudo);
-
-//        Board.Instance.ChangeStatusTiles(legal, TileStatus.Act);
-//    }
-
-//    public void HandleMove(Tile targetTile, Piece piece)
-//    {
-//        if (pickingPiece == null) return;
-
-//        var pseudo = pickingPiece.GeneratePseudoLegalMoves();
-//        var legal = FilterLegalMoves(pickingPiece, pseudo);
-
-//        BoardPosition targetPos = targetTile.GetBoardPotition();
-//        bool isLegal = legal.Exists(m => m.to.x == targetPos.x && m.to.y == targetPos.y);
-//        if (!isLegal) return;
-
-//        Move selectedMove = legal.Find(m => m.to.x == targetPos.x && m.to.y == targetPos.y);
-
-//        // --- EN PASSANT ---
-//        if (selectedMove.moveType == MoveType.EnPassant)
-//        {
-//            int dir = pickingPiece.Color == PlayerColor.White ? 1 : -1;
-//            BoardPosition pawnPos = new BoardPosition(selectedMove.to.x - dir, selectedMove.to.y);
-//            Piece enPassantPawn = GetPieceAt(pawnPos);
-//            if (enPassantPawn != null) { Destroy(enPassantPawn.gameObject); SetPieceAt(pawnPos, null); }
-//        }
-
-
-//        // --- CASTLING ---
-//        if (selectedMove.moveType == MoveType.Castling) HandleCastling(selectedMove);
-
-//        // --- CAPTURE ---
-//        Piece targetPiece = GetPieceAt(targetPos);
-//        if (targetPiece != null && targetPiece.Color != pickingPiece.Color)
-//            Destroy(targetPiece.gameObject);
-
-//        // --- MOVE ---
-//        pickingPiece.MoveTo(targetTile);
-//        SetPieceAt(targetPos, pickingPiece);
-
-//        lastMove = selectedMove;
-
-//        // --- PROMOTION ---
-//        if (pickingPiece is Pawn && (targetPos.x == 0 || targetPos.x == 7))
-//            PromotePawn(pickingPiece);
-
-//        pickingPiece = null;
-//        Board.Instance.ChangeStatusTiles(null, TileStatus.NoAct);
-//        currentTurn = currentTurn == PlayerColor.White ? PlayerColor.Black : PlayerColor.White;
-//    }
-
-//    // --- LEGAL MOVE FILTER ---
-//    public List<Move> FilterLegalMoves(Piece piece, List<Move> pseudoMoves)
-//    {
-//        List<Move> legal = new List<Move>();
-//        foreach (var mv in pseudoMoves)
-//        {
-//            Piece captured = SimulateMove(piece, mv);
-//            if (!IsKingInCheck(piece.Color)) legal.Add(mv);
-//            UndoSimulateMove(piece, mv, captured);
-//        }
-//        return legal;
-//    }
-
-//    private Piece SimulateMove(Piece piece, Move mv)
-//    {
-//        Piece captured = GetPieceAt(mv.to);
-//        SetPieceAt(mv.from, null);
-//        SetPieceAt(mv.to, piece);
-//        return captured;
-//    }
-
-//    private void UndoSimulateMove(Piece piece, Move mv, Piece captured)
-//    {
-//        SetPieceAt(mv.from, piece);
-//        SetPieceAt(mv.to, captured);
-//    }
-
-//    public bool IsKingInCheck(PlayerColor color)
-//    {
-//        Piece king = FindKing(color);
-//        if (king == null) return false;
-
-//        for (int row = 0; row < 8; row++)
-//        {
-//            for (int col = 0; col < 8; col++)
-//            {
-//                Piece p = pieces[row, col];
-//                if (p == null || p.Color == color) continue;
-
-//                foreach (var mv in p.GeneratePseudoLegalMoves())
-//                {
-//                    if (mv.to.x == king.Pos.x && mv.to.y == king.Pos.y)
-//                        return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-
-//    private Piece FindKing(PlayerColor color)
-//    {
-//        for (int row = 0; row < 8; row++)
-//            for (int col = 0; col < 8; col++)
-//            {
-//                Piece p = pieces[row, col];
-//                if (p != null && p.Color == color && p is King)
-//                    return p;
-//            }
-//        return null;
-//    }
-
-//    private void HandleCastling(Move mv)
-//    {
-//        int row = mv.from.x; // x = hàng
-//        // King side
-//        if (mv.to.y == 6)
-//        {
-//            Piece rook = GetPieceAt(new BoardPosition(row, 7));
-//            rook.MoveTo(Board.Instance.GetTile(row, 5));
-//            SetPieceAt(new BoardPosition(row, 5), rook);
-//            SetPieceAt(new BoardPosition(row, 7), null);
-//        }
-//        // Queen side
-//        else if (mv.to.y == 2)
-//        {
-//            Piece rook = GetPieceAt(new BoardPosition(row, 0));
-//            rook.MoveTo(Board.Instance.GetTile(row, 3));
-//            SetPieceAt(new BoardPosition(row, 3), rook);
-//            SetPieceAt(new BoardPosition(row, 0), null);
-//        }
-//    }
-
-//    private void PromotePawn(Piece pawn)
-//    {
-//        Destroy(pawn.gameObject);
-//        Piece promotePawn = Instantiate(Board.Instance.PrefabLibrary.GetPrefab(PieceType.Queen, pawn.Color));
-//        promotePawn.Color = pawn.Color;
-//        promotePawn.MoveTo(pawn.Tile);
-//        SetPieceAt(pawn.Pos, promotePawn);
-//    }
-
-//    public void SetPieceAt(BoardPosition pos, Piece p)
-//    {
-//        if (!pos.InBounds()) return;
-//        pieces[pos.x, pos.y] = p; // x = row, y = col
-//    }
-
-//    public Piece GetPieceAt(BoardPosition pos)
-//    {
-//        if (!pos.InBounds()) return null;
-//        return pieces[pos.x, pos.y]; // x = row, y = col
-//    }
-//}
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BoardManager : MonoBehaviour
 {
+    public const int BOARD_SIZE = 8;
+
     #region Singleton
     public static BoardManager Instance { get; private set; }
     private void Awake()
@@ -203,15 +16,22 @@ public class BoardManager : MonoBehaviour
     }
     #endregion
 
-    [SerializeField] private Piece[,] pieces = new Piece[8, 8];
+    [SerializeField] private Piece[,] pieces = new Piece[BOARD_SIZE, BOARD_SIZE];
 
     private Piece pickingPiece;
     private PlayerColor currentTurn = PlayerColor.White;
     private Move lastMove;
+    
+    private Piece whiteKing;
+    private Piece blackKing;
+
+    // Cache list to avoid GC allocation
+    private List<Move> cachedLegalMoves = new List<Move>(64);
+    private List<Move> cachedPseudoMoves = new List<Move>(64);
 
     public Piece PickingPiece { get => pickingPiece; set => pickingPiece = value; }
     public PlayerColor CurrentTurn { get => currentTurn; set => currentTurn = value; }
-    public Move LastMove => lastMove; // expose LastMove
+    public Move LastMove => lastMove;
 
     private void OnEnable() => Piece.OnAnyPieceClicked += HandlePieceClick;
     private void OnDisable() => Piece.OnAnyPieceClicked -= HandlePieceClick;
@@ -221,21 +41,30 @@ public class BoardManager : MonoBehaviour
         if (clickedPiece.Color != currentTurn) return;
 
         pickingPiece = clickedPiece;
-        var pseudo = clickedPiece.GeneratePseudoLegalMoves();
-        var legal = FilterLegalMoves(clickedPiece, pseudo);
+        
+        // Use cached lists
+        cachedPseudoMoves.Clear();
+        clickedPiece.GeneratePseudoLegalMoves(cachedPseudoMoves);
+        
+        cachedLegalMoves.Clear();
+        FilterLegalMoves(clickedPiece, cachedPseudoMoves, cachedLegalMoves);
 
-        Board.Instance.ChangeStatusTiles(legal, TileStatus.Act);
+        Board.Instance.ChangeStatusTiles(cachedLegalMoves, TileStatus.Act);
     }
 
     public void HandleMove(Tile targetTile, Piece piece)
     {
         if (pickingPiece == null) return;
 
-        var pseudo = pickingPiece.GeneratePseudoLegalMoves();
-        var legal = FilterLegalMoves(pickingPiece, pseudo);
+        cachedPseudoMoves.Clear();
+        pickingPiece.GeneratePseudoLegalMoves(cachedPseudoMoves);
+        
+        cachedLegalMoves.Clear();
+        FilterLegalMoves(pickingPiece, cachedPseudoMoves, cachedLegalMoves);
 
         BoardPosition targetPos = targetTile.GetBoardPotition();
-        Move selectedMove = legal.Find(m => m.to.x == targetPos.x && m.to.y == targetPos.y);
+        Move selectedMove = cachedLegalMoves.Find(m => m.to.x == targetPos.x && m.to.y == targetPos.y);
+        
         if (selectedMove == null) return;
 
         // --- EN PASSANT ---
@@ -257,7 +86,6 @@ public class BoardManager : MonoBehaviour
         // --- CAPTURE ---
         if (selectedMove.IsCapture())
         {
-            Debug.Log("Destroy");
             Piece targetPiece = GetPieceAt(targetPos);
             if (targetPiece != null && targetPiece.Color != pickingPiece.Color)
             {
@@ -273,7 +101,7 @@ public class BoardManager : MonoBehaviour
         lastMove = selectedMove;
 
         // --- PROMOTION ---
-        if (pickingPiece is Pawn && (targetPos.x == 0 || targetPos.x == 7))
+        if (pickingPiece is Pawn && (targetPos.x == 0 || targetPos.x == BOARD_SIZE - 1))
             PromotePawn(pickingPiece);
 
         pickingPiece = null;
@@ -284,16 +112,19 @@ public class BoardManager : MonoBehaviour
     }
 
     // --- LEGAL MOVE FILTER ---
-    public List<Move> FilterLegalMoves(Piece piece, List<Move> pseudoMoves)
+    // Modified to be non-allocating
+    public void FilterLegalMoves(Piece piece, List<Move> pseudoMoves, List<Move> resultMoves)
     {
-        List<Move> legal = new List<Move>();
+        resultMoves.Clear();
         foreach (var mv in pseudoMoves)
         {
             Piece captured = SimulateMove(piece, mv);
-            if (!IsKingInCheck(piece.Color)) legal.Add(mv);
+            if (!IsKingInCheck(piece.Color)) 
+            {
+                resultMoves.Add(mv);
+            }
             UndoSimulateMove(piece, mv, captured);
         }
-        return legal;
     }
 
     private Piece SimulateMove(Piece piece, Move mv)
@@ -312,35 +143,118 @@ public class BoardManager : MonoBehaviour
 
     public bool IsKingInCheck(PlayerColor color)
     {
-        Piece king = FindKing(color);
+        Piece king = GetKing(color);
         if (king == null) return false;
 
-        for (int row = 0; row < 8; row++)
-        {
-            for (int col = 0; col < 8; col++)
-            {
-                Piece p = pieces[row, col];
-                if (p == null || p.Color == color) continue;
+        PlayerColor enemyColor = (color == PlayerColor.White) ? PlayerColor.Black : PlayerColor.White;
+        return IsSquareAttacked(king.Pos, enemyColor);
+    }
 
-                foreach (var mv in p.GeneratePseudoLegalMoves())
+    /// <summary>
+    /// Checks if a specific square is under attack by the opponent using Raycasting (Reverse Check).
+    /// This is O(1) relative to board size (constant checks), much faster than iterating all pieces O(N).
+    /// </summary>
+    public bool IsSquareAttacked(BoardPosition pos, PlayerColor attackerColor)
+    {
+        // 1. Check for Sliding Pieces (Rook, Queen) - Vertical/Horizontal
+        if (CheckSlidingAttack(pos, attackerColor, new int[] { 1, -1, 0, 0 }, new int[] { 0, 0, 1, -1 }, PieceType.Rook))
+            return true;
+
+        // 2. Check for Sliding Pieces (Bishop, Queen) - Diagonal
+        if (CheckSlidingAttack(pos, attackerColor, new int[] { 1, 1, -1, -1 }, new int[] { 1, -1, 1, -1 }, PieceType.Bishop))
+            return true;
+
+        // 3. Check for Knights
+        int[] knightDx = { 1, 1, 2, 2, -1, -1, -2, -2 };
+        int[] knightDy = { 2, -2, 1, -1, 2, -2, 1, -1 };
+        for (int i = 0; i < 8; i++)
+        {
+            if (CheckSinglePieceAttack(pos, attackerColor, knightDx[i], knightDy[i], PieceType.Knight))
+                return true;
+        }
+
+        // 4. Check for Pawns
+        // White pawns move +1 x, so they attack from x-1. Black pawns move -1 x, so they attack from x+1.
+        int pawnAttackRowDir = (attackerColor == PlayerColor.White) ? -1 : 1; 
+        if (CheckSinglePieceAttack(pos, attackerColor, pawnAttackRowDir, 1, PieceType.Pawn) ||
+            CheckSinglePieceAttack(pos, attackerColor, pawnAttackRowDir, -1, PieceType.Pawn))
+            return true;
+
+        // 5. Check for King (enemy king logic, kings cannot stand next to each other)
+        int[] kingDx = { 1, 1, 1, 0, 0, -1, -1, -1 };
+        int[] kingDy = { 1, 0, -1, 1, -1, 1, 0, -1 };
+        for (int i = 0; i < 8; i++)
+        {
+            if (CheckSinglePieceAttack(pos, attackerColor, kingDx[i], kingDy[i], PieceType.King))
+                return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckSlidingAttack(BoardPosition startPos, PlayerColor attackerColor, int[] dx, int[] dy, PieceType straightType)
+    {
+        for (int i = 0; i < dx.Length; i++)
+        {
+            for (int dist = 1; dist < BOARD_SIZE; dist++)
+            {
+                int x = startPos.x + dx[i] * dist;
+                int y = startPos.y + dy[i] * dist;
+
+                if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) break;
+
+                Piece p = pieces[x, y];
+                if (p != null)
                 {
-                    if (mv.to.x == king.Pos.x && mv.to.y == king.Pos.y)
+                    if (p.Color == attackerColor && (p.PieceType == straightType || p.PieceType == PieceType.Queen))
+                    {
                         return true;
+                    }
+                    // Blocked by any piece (friend or foe that isn't the attacker we looked for)
+                    break;
                 }
             }
         }
         return false;
     }
 
+    private bool CheckSinglePieceAttack(BoardPosition startPos, PlayerColor attackerColor, int dx, int dy, PieceType targetType)
+    {
+        int x = startPos.x + dx;
+        int y = startPos.y + dy;
+
+        if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) return false;
+
+        Piece p = pieces[x, y];
+        return p != null && p.Color == attackerColor && p.PieceType == targetType;
+    }
+
+    private Piece GetKing(PlayerColor color)
+    {
+        Piece king = color == PlayerColor.White ? whiteKing : blackKing;
+        if (king != null && king is King && king.Color == color)
+            return king;
+        
+        king = FindKing(color);
+        if (color == PlayerColor.White)
+            whiteKing = king;
+        else
+            blackKing = king;
+        
+        return king;
+    }
+
     private Piece FindKing(PlayerColor color)
     {
-        for (int row = 0; row < 8; row++)
-            for (int col = 0; col < 8; col++)
+        for (int row = 0; row < BOARD_SIZE; row++)
+        {
+            for (int col = 0; col < BOARD_SIZE; col++)
             {
                 Piece p = pieces[row, col];
                 if (p != null && p.Color == color && p is King)
                     return p;
             }
+        }
         return null;
     }
 
@@ -354,7 +268,7 @@ public class BoardManager : MonoBehaviour
         // King side castling
         if (mv.to.y == 6)
         {
-            rookFromPos = new BoardPosition(row, 7);
+            rookFromPos = new BoardPosition(row, BOARD_SIZE - 1);
             rookToPos = new BoardPosition(row, 5);
         }
         // Queen side castling
@@ -388,7 +302,25 @@ public class BoardManager : MonoBehaviour
     public void SetPieceAt(BoardPosition pos, Piece p)
     {
         if (!pos.InBounds()) return;
-        pieces[pos.x, pos.y] = p; // x = row, y = col
+        
+        Piece oldPiece = pieces[pos.x, pos.y];
+        pieces[pos.x, pos.y] = p;
+        
+        if (oldPiece is King)
+        {
+            if (oldPiece.Color == PlayerColor.White)
+                whiteKing = null;
+            else
+                blackKing = null;
+        }
+        
+        if (p is King)
+        {
+            if (p.Color == PlayerColor.White)
+                whiteKing = p;
+            else
+                blackKing = p;
+        }
     }
 
     public Piece GetPieceAt(BoardPosition pos)
@@ -397,7 +329,6 @@ public class BoardManager : MonoBehaviour
         return pieces[pos.x, pos.y]; // x = row, y = col
     }
 
-    // --- HELPER LAST MOVE CHECK ---
     public bool LastMoveWasDoubleStep()
     {
         return lastMove != null && lastMove.IsDoubleStep();
@@ -405,7 +336,10 @@ public class BoardManager : MonoBehaviour
 
     public bool LastMoveWasEnPassantablePawn()
     {
-        return lastMove != null && lastMove.IsDoubleStep();
+        if (lastMove == null || !lastMove.IsDoubleStep()) return false;
+        
+        BoardPosition lastMoveTo = lastMove.to;
+        Piece lastMovedPiece = GetPieceAt(lastMoveTo);
+        return lastMovedPiece != null && lastMovedPiece is Pawn;
     }
 }
-
